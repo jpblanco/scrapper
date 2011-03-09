0043f7a0c43add2521b8e5e587fbb3d2a8ccfa02
Juan Pablo Blanco <jp.blanco@moove-it.com>
2011-03-09 16:30:26 -0200

    fixed bug

--- scrapper.rb
+++ scrapper.rb
@@ -1,4 +1,7 @@
 require 'nokogiri'
+require 'open-uri'
+require 'net/http'
+require 'net/https'
 
 ANDROID_HOST="https://market.android.com"
 
@@ -12,9 +15,20 @@ class App
   attr_accessor :size
 end
 
+def get_page(url)
+  url = URI.parse(url)
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  document = ""
+  http.start {
+    http.request_get(url.path + '?' + url.query) {|res| document = res.body }
+  }	
+  document
+end
+
 def obtain_app_details(app)
   url = app.url
-  doc = Nokogiri::HTML(%x(curl -s -X GET #{url}))
+  doc = Nokogiri::HTML(get_page(url))
 
   app.price = doc.css("dl.doc-metadata-list > dd:last-child").first.content
   downloads = doc.css("dl.doc-metadata-list > dd")[5].content.split(" - ")
@@ -35,15 +49,12 @@ def search(query, options ={})
   real_sort = { :relevance => "sort=1",
                 :popularity => "sort=0" }
 
-  print "Cargando "
+  puts "Cargando "
 
   (1..options[:pages]).each do |page|
-    url ="#{ANDROID_HOST}/search?q=#{query}&c=apps&#{real_filters[default_options[:filter].to_sym]}&#{real_sort[default_options[:sort].to_sym]}&start=#{(page-1)*12}&num=12"
-    print url if page == 1 
-    puts ""
+    url ="#{ANDROID_HOST}/search?q=#{query}&c=apps&#{real_filters[options[:filter].to_sym]}&#{real_sort[options[:sort].to_sym]}&start=#{(page-1)*12}&num=12"
 
-    #BUG: it seems I only get the same html for the different pages, no idea why.
-    doc = Nokogiri::HTML(%x(curl -s -X GET #{url}))
+    doc = Nokogiri::HTML(get_page(url))
 
     no_results = !(doc.css(".no-results-section").empty?)
 
@@ -71,45 +82,51 @@ end
 
 #usage: ruby scrapper.rb query --filter [free|paid] --sort [relevance|popularity]
 
-query = ARGV.first
+def start_scrapping
+  query = ARGV.first
 
-if query.nil?
-  puts "Lo estas haciendo mal!"
-  puts "Sintaxis: ruby scrapper.rb query --filter [free|paid] --sort [relevance|popularity]"
-else
-  filter = nil
-  sort = nil
-  pages = 5
-  start_time = Time.now
+  if query.nil?
+    puts "Lo estas haciendo mal!"
+    puts "Sintaxis: ruby scrapper.rb query --filter [free|paid] --sort [relevance|popularity]"
+  else
+    filter = "no_filter"
+    sort = nil
+    pages = 2
+    start_time = Time.now
+    sort = "relevance"
 
-  filter = ARGV[(ARGV.index "--filter") + 1]  unless (ARGV.index "--filter").nil?
-  sort = ARGV[(ARGV.index "--sort") + 1]  unless (ARGV.index "--sort").nil?
-  pages = ARGV[(ARGV.index "--pages") + 1].to_i  unless (ARGV.index "--pages").nil?
+    filter = ARGV[(ARGV.index "--filter") + 1]  unless (ARGV.index "--filter").nil?
+    sort = ARGV[(ARGV.index "--sort") + 1]  unless (ARGV.index "--sort").nil?
+    pages = ARGV[(ARGV.index "--pages") + 1].to_i  unless (ARGV.index "--pages").nil?
 
-  apps = search(query, pages, {:filter => filter, :sort => sort})
+    apps = search(query, {:filter => filter, :sort => sort, :pages => pages})
 
-  puts ""
-  puts "Obteniendo datos de las aplicaciones."
-  puts ""
-
-  apps.each do |app|
-    print "#{app.name}   "
-    prev_time = Time.now
-    obtain_app_details app
-    print "(#{Time.now-prev_time} s)"
     puts ""
-  end
-
-  open("#{query}-apps.csv", "a") { |f|
-    f.puts "Nombre,Url,Precio,DwnMin,DwnMax,DwnAvg,Espacio"
+    puts "Obteniendo datos de las aplicaciones."
+    puts ""
 
     apps.each do |app|
-      f.puts "#{app.name},#{app.url},#{app.price},#{app.downloads_min},#{app.downloads_max},#{app.downloads_avg},#{app.size}"
+      print "#{app.name}   "
+      prev_time = Time.now
+      obtain_app_details app
+      print "(#{Time.now-prev_time} s)"
+      puts ""
     end
-  }
 
-  puts ""
-  puts "Finalizado en #{Time.now - start_time} s"
+    open("#{query}-apps.csv", "a") { |f|
+      f.puts "Nombre,Url,Precio,DwnMin,DwnMax,DwnAvg,Espacio"
+
+      apps.each do |app|
+        f.puts "#{app.name},#{app.url},#{app.price},#{app.downloads_min},#{app.downloads_max},#{app.downloads_avg},#{app.size}"
+      end
+    }
+
+    puts ""
+    puts "Finalizado en #{Time.now - start_time} s"
+  end
+
 end
+
+start_scrapping
 
 
